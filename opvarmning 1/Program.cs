d5f2303f0e6148d2246f645792dd526a71e639af
benjamin ostner <benjamin110101@gmail.com>
2018-03-13 08:50:41 +0100

    done

--- opvarmning 1/Program.cs
+++ opvarmning 1/Program.cs
@@ -10,6 +10,25 @@ namespace opvarmning_1
     {
         static void Main(string[] args)
         {
+            string svar;
+            do
+            {
+                Console.WriteLine("Skriv dit navn");
+                string navn = Console.ReadLine();
+
+                Console.WriteLine("Skriv dit efternavn");
+                string efternavn = Console.ReadLine();
+                
+                Console.WriteLine("Skriv din alder");
+                string alder = Console.ReadLine();
+                
+
+                Console.WriteLine("Dit navn er " + navn + " " + efternavn + " og du er " + alder + " år gammel");
+
+
+                Console.WriteLine("vil du prøve igen J/N");
+                svar = Console.ReadLine();
+            } while (svar == "J" || svar == "j") ;
         }
     }
 }
